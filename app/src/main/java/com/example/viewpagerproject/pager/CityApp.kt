d3a5fc7123578e64cd69e255cd8e1253e751fb28
Rozzi21 <fakhruddinarrozi@gmail.com>
2026-04-28 14:39:59 +0700

    add city App for main screen

--- app/src/main/java/com/example/viewpagerproject/pager/CityApp.kt
+++ app/src/main/java/com/example/viewpagerproject/pager/CityApp.kt
@@ -1,2 +1,15 @@
 package com.example.viewpagerproject.pager
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import com.example.viewpagerproject.components.CityList
+import com.example.viewpagerproject.data.CityDataResource
+
+@Composable
+
+fun CityApp() {
+    Column {
+        CityTabCarousel() // viewpager
+        CityList(cityList = CityDataResource().loadCities()) // LazyList
+    }
+}
--- app/src/main/java/com/example/viewpagerproject/pager/CityTabCarousel.kt
+++ app/src/main/java/com/example/viewpagerproject/pager/CityTabCarousel.kt
@@ -1,6 +1,5 @@
 package com.example.viewpagerproject.pager
 
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
